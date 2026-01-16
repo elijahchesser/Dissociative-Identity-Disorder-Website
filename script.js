46e228101fc2d6b7f923d14c021193d72633a43e
Elainaisyes <elijah.chesser@students.cvtech.edu>
2026-01-16 12:52:59 -0600

    Updated

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-    const forumsContainer = document.getElementById('forumsContainer');
+    const forumItemsContainer = document.getElementById('forumItemsContainer');
     const submitButton = document.getElementById('submitButton');
 
     submitButton.addEventListener('click', function() {
+        user = document.getElementById('newPostUser').value.trim();
+        text = document.getElementById('newPostText').value.trim();
+        // if (user === '' || text === '') {
+        //     alert('Please fill in both fields.');
+        //     return;
+        // }
+
         const item = document.createElement('div');
         item.className = 'forumItem';
         
         const username = document.createElement('h2');
-        username.textContent = 'EvilKiwi:';
+        username.textContent = user + ': ';
         item.appendChild(username);
 
         const message = document.createElement('p');
-        message.textContent = 'This is a new forum post.';
+        message.textContent = text;
         item.appendChild(message);
-        forumsContainer.appendChild(item);
+        forumItemsContainer.appendChild(item);
+
+        newPostUser.value = '';
+        newPostText.value = '';
     });
 });
